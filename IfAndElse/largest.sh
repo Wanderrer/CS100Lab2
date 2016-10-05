1cb0f1a423e8335da663fe7bef5a7fef9a68a16b
Rick Boshae <rbosh001@hammer.cs.ucr.edu>
2016-10-05 02:11:52 -0700

    Completed ifandelse part of the assignment

--- IfAndElse/largest.sh
+++ IfAndElse/largest.sh
@@ -9,22 +9,28 @@ secondNumber=$2
 thirdNumber=$3
 tooManyNumbers=$4
 
-if  [ $1 = "" ] || [ $2 = "" ] || [ $3 = "" ]; then
+if  [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
 	echo "Please make sure to enter exactly three values"
 	echo "Exit 1"
 	exit
 fi
 
+if [ -z $4 ]; then
+
 	 
-largestNumber=$firstNumber
+	largestNumber=$firstNumber
 
-if [ "$secondNumber" -gt "$largestNumber" ]; then
-	largestNumber=$secondNumber
+	if [ "$secondNumber" -gt "$largestNumber" ]; then
+		largestNumber=$secondNumber
+	fi
+
+	if [ "$thirdNumber" -gt "$largestNumber" ]; then
+		largestNumber=$thirdNumber
+	fi
+
+	echo $largestNumber
+else
+	echo "Too many parameters have been passed, please make sure to enter exactly three values"
+	echo "Exit 2"
 fi
-
-if [ "$thirdNumber" -gt "$largestNumber" ]; then
-	largestNumber=$thirdNumber
-fi
-
-echo $largestNumber
 
